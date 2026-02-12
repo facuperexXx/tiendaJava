37eede8f876ffa81acc0ad2bb9ccff03bd6d5ca7
facundo <00facuperez@gmail.com>
2026-02-12 13:06:11 -0300

    se agrega clase base repositorio para contenedores

---
+++ src/repositorios/Repositorio.java
@@ -0,0 +1,45 @@
+package repositorios;
+
+import java.util.List;
+
+/*
+    - Falta la funcion de modificar datos.
+ */
+
+public abstract class Repositorio<T> {
+    protected List<T> datos;
+
+    protected abstract int identificador(T registro);
+
+    // METODOS CRUD
+    public void agregar(T nuevo) {
+        if(!checkRegistro(identificador(nuevo))) {
+            datos.add(nuevo);
+        }
+    }
+
+    public T buscar(int id) {
+        T encontrado = null;
+        for(T e : datos) {
+            if(identificador(e) == id) {
+                encontrado = e;
+            }
+        }
+        return encontrado;
+    }
+
+    public void borrar(int id) {
+        T registro = buscar(id);
+        datos.remove(registro);
+    }
+
+    // METODOS AUXILIARES
+    public boolean checkRegistro(int id) {
+        // True -> Existe en base de datos.
+        Boolean check = false;
+        if(buscar(id) != null) {
+            check = true;
+        }
+        return check;
+    }
+}
